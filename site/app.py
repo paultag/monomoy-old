acc6cb2317702dca6ca3a4d747fae90ea62b8ce6
Paul Tagliamonte <paultag@sunlightfoundation.com>
2012-08-08 08:43:04 -0400

    More and more!

--- site/app.py
+++ site/app.py
@@ -2,11 +2,12 @@
 # and conditions of the Expat license, a copy of which should be given to you
 # with the source of this application.
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 from monomoy.db import db
 
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
@@ -16,9 +17,11 @@ def index():
         "jobs": jobs
     })
 
+
 @app.route("/about")
 def about():
     return render_template('about.html', **{})
+
 
 if __name__ == "__main__":
     app.debug = True
